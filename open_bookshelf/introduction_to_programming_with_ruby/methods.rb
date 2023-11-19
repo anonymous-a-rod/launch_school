171e0f3546170f6c448d545f294132185145fd29
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-11-19 09:35:24 -0500

    methods

---
+++ open_bookshelf/introduction_to_programming_with_ruby/methods.rb
@@ -0,0 +1,35 @@
+# exercise 1
+
+def greeting(name)
+  "Hello, #{name}"
+end
+
+puts greeting("Aaron")
+
+# exercise 2
+
+# 1. 2
+# 2. nil
+# 3. "Joe"
+# 4. "four"
+# 5. nil
+
+# exercise 3
+
+def multiply(x, y)
+  x * y
+end
+
+puts multiply(2, 4)
+
+# exercise 4
+
+# prints nothing to the screen
+
+# exercise 5
+
+# returns nil
+
+# exercise 6
+
+# only provided 1 of 2 arguments when calling calculate_product
