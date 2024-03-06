1ef4659e6981bfc44c64ffbb063a604eaef2a7a6
Aaron <aaron1venema@gmail.com>
2024-03-05 20:19:49 -0500

    Create variable_scope.rb

---
+++ ruby_basics/variable_scope.rb
@@ -0,0 +1,102 @@
+# a = 'hello'
+
+# def my_value(b)
+#   b += '!'
+# end
+
+# my_value(a)
+
+# puts a
+
+# a = 7
+
+# def my_value(a)
+#   a += 10
+#   puts a
+# end
+
+# my_value(a)
+# puts a
+
+# a = 7
+
+# def my_value(b)
+#   a = b
+# end
+
+# my_value(a + 5)
+# puts 
+
+# a = "Xyzzy"
+
+# def my_values(b)
+#   b[2] = '-'
+# end
+
+# my_values(a)
+# puts a
+
+# a = [1,2,3]
+# b = a
+# b << 4
+# # a = 'turd'
+
+# p a
+# p b
+
+# a = "Xyzzy"
+
+# def my_value(b)
+#   b = 'yzzyX'
+# end
+
+# my_value(a)
+# puts a
+
+# a = 7
+
+# def my_value(b)
+#   b = a + a
+# end
+
+# my_value(a)
+# puts a
+
+# a = 7
+# array = [1, 2, 3]
+
+# array.each do |element|
+#   a = element
+# end
+
+# puts a
+
+# array = [1, 2, 3]
+# a = 0
+# array.each do |element|
+#   a = element
+# end
+
+# puts a
+
+# a = 7
+# array = [1, 2, 3]
+
+# array.each do |a|
+#   a += 1
+# end
+
+# puts a
+
+a = 7
+array = [1, 2, 3]
+
+def my_value(ary)
+  ary.each do |b|
+    a += b
+  end
+end
+
+my_value(array)
+puts a
+
