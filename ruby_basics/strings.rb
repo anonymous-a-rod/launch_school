a5c3f3eff92adaaea59a8c4d9431008930b7e311
Aaron <aaron1venema@gmail.com>
2024-03-23 15:10:41 -0400

    strings

---
+++ ruby_basics/strings.rb
@@ -0,0 +1,52 @@
+# empty_string = ""
+# string = String.new
+
+# puts "It's now 12 o'clock."
+
+# name = 'RoGeR'
+# name = 'Dave'
+
+# puts name.downcase == 'DAVE'
+
+# name = 'Elizabeth'
+
+# puts "Hello, #{name}!"
+
+# first_name = 'John'
+# last_name = 'Doe'
+
+# fullname = "#{first_name} #{last_name}"
+# fullname = first_name + ' ' + last_name
+# puts fullname
+
+# state = 'tExAs'
+# state.capitalize!
+# puts state
+
+# .downcase
+# .upcase
+# .capitalize
+
+# use ! to modify the caller
+
+# greeting = 'Hello!'
+# greeting.gsub!('Hello!', 'Goodbye!')
+# puts greeting
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# puts alphabet.split('')
+
+
+# words = 'car human elephant airplane'
+
+# words.split.each
+# do |word|
+#     puts word + 's'
+# end
+
+colors = 'blue pink yellow orange'
+
+puts colors.include?('yellow')
+puts colors.include?('purple')
+
