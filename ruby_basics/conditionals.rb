57b73d776b92e20597719e2ecada7b04c20239c3
Aaron <aaron1venema@gmail.com>
2024-03-24 20:25:42 -0400

    easy ruby problems

---
+++ ruby_basics/conditionals.rb
@@ -0,0 +1,20 @@
+# sun = ['visible', 'hidden'].sample
+
+# puts "The sun is so bright!" if sun == 'visible'
+# puts "The sun is so bright!" unless sun == 'visible'
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+when 'yellow'
+when 'red'
+end
